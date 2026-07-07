51df7e0090c6b13fade46f58aa845ca06158b02e
drahsandev <drahsandev@gmail.com>
2026-07-07 18:31:18 +0500

    rectangular shape

---
+++ rectangularshape.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int rows, cols;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+    for ( int i = 1; i <= rows; i++)
+    {
+        printf("\n");
+        for(int j =1; j<=cols; j++)
+        {
+            printf("&");
+        }
+    }
+    
+}
